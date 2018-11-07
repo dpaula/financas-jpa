c805a34cac9c07985cc149562028e499900678e1
dpaula <fernando.dpaula@gmail.com>
2018-11-06 22:18:53 -0200

    classe para testar a inclusão da entidade conta

---
+++ src/br/com/lima/teste/TestaConta.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package br.com.lima.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.lima.modelo.Conta;
+
+/**
+ * @author ferna
+ *
+ */
+public class TestaConta {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Conta conta = new Conta();
+
+		conta.setAgencia("0147");
+		conta.setBanco("Santander");
+		conta.setNumero("010243938");
+		conta.setTitular("Fernando de Lima");
+
+		// criar uma entidade de persistencia usando as configurações declaradas no
+		// arquivo persistence.xml
+		EntityManagerFactory emFabrica = Persistence.createEntityManagerFactory("financasdb");
+		EntityManager em = emFabrica.createEntityManager();
+
+		// sempre se inicia uma transação
+		em.getTransaction().begin();
+
+		// realiza a persistencia da entidade
+		em.persist(conta);
+
+		// e comita a transação no final
+		em.getTransaction().commit();
+
+		// não implementam Closeble, então sempre tem q fechar a sessão
+		em.close();
+		emFabrica.close();
+
+	}
+
+}
