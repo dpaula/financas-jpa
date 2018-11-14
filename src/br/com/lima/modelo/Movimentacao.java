b800f14e17ef40bfe7f977db528c740220d309bd
dpaula <fernando.dpaula@gmail.com>
2018-11-14 21:31:35 -0200

    nova entidade com atributos mapeados para enumerador e timestamp

---
+++ src/br/com/lima/modelo/Movimentacao.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package br.com.lima.modelo;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * @author ferna
+ *
+ */
+@Entity
+public class Movimentacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private BigDecimal valor;
+
+	// definindo um enumerador
+	@Enumerated(EnumType.STRING)
+	private TipoMovimentacao tipo;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar data;
+	private String descricao;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor
+	 *            the valor to set
+	 */
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	/**
+	 * @return the tipo
+	 */
+	public TipoMovimentacao getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * @param tipo
+	 *            the tipo to set
+	 */
+	public void setTipo(TipoMovimentacao tipo) {
+		this.tipo = tipo;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public Calendar getData() {
+		return data;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao
+	 *            the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
---
+++ src/br/com/lima/modelo/TipoMovimentacao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package br.com.lima.modelo;
+
+/**
+ * @author ferna
+ *
+ */
+public enum TipoMovimentacao {
+
+	ENTRADA, SAIDA;
+
+}
