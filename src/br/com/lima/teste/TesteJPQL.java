1fb30ec9b134ebb76a8c60f1b663eac53052b3f6
dpaula <fernando.dpaula@gmail.com>
2018-11-15 21:09:02 -0200

    usando parametros nas JPQL

--- src/br/com/lima/teste/TesteJPQL.java
+++ src/br/com/lima/teste/TesteJPQL.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import br.com.lima.modelo.Conta;
 import br.com.lima.modelo.Movimentacao;
 import br.com.lima.util.JPAUtil;
 
@@ -25,10 +26,14 @@ public class TesteJPQL {
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
+		Conta conta = new Conta();
+		conta.setId(1);
+
 		// criando uma jpql sempre acessando os campos da tabela como atributos
-		// (conta.id)
-		String jpql = "select m from Movimentacao m where m.conta.id = 1";
+		String jpql = "select m from Movimentacao m where m.conta = :pConta";
 		Query query = em.createQuery(jpql);
+		// setando parametros objeto
+		query.setParameter("pConta", conta);
 
 		// retorna a lista dos registros
 		List<Movimentacao> movis = query.getResultList();
