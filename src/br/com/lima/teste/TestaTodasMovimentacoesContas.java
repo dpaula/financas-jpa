f801a76cc4d461e2a9a09593cca0e2b5ea0f4947
Fernando de Lima <fernando.dpaula@gmail.com>
2018-11-18 18:53:15 -0200

    usando fetch na jpql para evitar o comportamento Lazy

--- src/br/com/lima/teste/TestaTodasMovimentacoesContas.java
+++ src/br/com/lima/teste/TestaTodasMovimentacoesContas.java
@@ -15,25 +15,22 @@ public class TestaTodasMovimentacoesContas {
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
-		String jpql = "select c from Conta c";
+		// Porém, podemos não querer isto e, pela quantidade de queries feitas, queremos
+		// que quando a conta for buscada, sejam trazidas também as movimentações.
+		// Então, precisaremos substituir o comportamento Lazy por Eager Loading, a
+		// partir do parâmetro join fetch, o que quer dizer que queremos juntar, nesta
+		// query, a conta e a movimentação.
+		String jpql = "select c from Conta c join fetch c.movimentacoes";
 		Query query = em.createQuery(jpql);
 
 		List<Conta> contas = query.getResultList();
 
 		for (Conta conta : contas) {
 			System.out.println("Titular conta: " + conta.getTitular());
-			// N + 1, por realizar 1 select para buscar as contas e, para cada uma delas, no
-			// laço, foi feito mais um select para buscar as movimentações correspondentes.
+			// então agora será feito apenas um select, e naõ um para cada conta
 			System.out.println("    Movimentacoes: ");
 			System.out.println(conta.getMovimentacoes());
 		}
-
-		// Isso ocorre justamente porque, por padrão, os relacionamentos [...]ToMany,
-		// que são listas ou conjuntos, são considerados Lazy, com "carregamento
-		// preguiçoso".
-		// Ou seja, o JPA só irá ao banco para buscar estes relacionamentos quando você
-		// precisa deles,
-		// ao chamarmos o método getMovimentacoes(), por exemplo.
 
 		em.getTransaction().commit();
 		em.close();
